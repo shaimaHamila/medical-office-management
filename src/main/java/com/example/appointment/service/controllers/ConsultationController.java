4280251ad3b0015df1a84767131b18530fe63ea2
Shaima Hamila <hamilachaima18@gmail.com>
2023-12-09 00:10:38 +0100

    CRUD consultations

--- src/main/java/com/example/appointment/service/controllers/ConsultationController.java
+++ src/main/java/com/example/appointment/service/controllers/ConsultationController.java
@@ -1,4 +1,62 @@
 package com.example.appointment.service.controllers;
 
+import com.example.appointment.persistance.dao.ConsultationRepository;
+import com.example.appointment.persistance.models.Consultation;
+import com.example.appointment.service.serviceInterface.ConsultationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/api/v1/consultations")
 public class ConsultationController {
+    @Autowired
+    ConsultationService consultationService;
+
+    @GetMapping
+    public ResponseEntity<List<Consultation>> getAllConsultations(){
+        List<Consultation> consultations = consultationService.getAllConsultations();
+        return new ResponseEntity<>(consultations, HttpStatus.OK);
+    }
+    @GetMapping("/{conId}")
+    public ResponseEntity<Consultation> getConsultationById(@PathVariable("conId") Long conId){
+        Consultation consultation = consultationService.getConsultationById(conId);
+        if (consultation != null){
+            return new ResponseEntity<>(consultation, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+    @PostMapping("/new")
+    public ResponseEntity<Consultation> saveConsultation(@RequestBody Consultation consultation){
+        Consultation newCon = consultationService.saveConsultation(consultation);
+        return new ResponseEntity<>(newCon, HttpStatus.CREATED);
+    }
+    @PutMapping("/{conId}/edit")
+    public ResponseEntity<Consultation> updateConsultation(@PathVariable("conId") Long conId, @RequestBody Consultation consultation){
+        Consultation toUpdateCon = consultationService.getConsultationById(conId);
+        if (toUpdateCon != null){
+            toUpdateCon.setDateCons(consultation.getDateCons());
+            toUpdateCon.setRecapCons(consultation.getRecapCons());
+            Consultation newCon = consultationService.saveConsultation(toUpdateCon);
+            return new ResponseEntity<>(newCon, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{conId}/delete")
+    public ResponseEntity<Long> updateConsultation(@PathVariable("conId") Long conId){
+        Consultation toDeleteCon = consultationService.getConsultationById(conId);
+        if (toDeleteCon != null){
+            consultationService.deleteConsultation(conId);
+            return new ResponseEntity<>(conId, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/example/appointment/service/serviceImp/ConsultationServiceImp.java
+++ src/main/java/com/example/appointment/service/serviceImp/ConsultationServiceImp.java
@@ -1,8 +1,36 @@
 package com.example.appointment.service.serviceImp;
 
-import com.example.appointment.service.serviceInterface.ConsultaionService;
+import com.example.appointment.persistance.dao.ConsultationRepository;
+import com.example.appointment.persistance.models.Consultation;
+import com.example.appointment.service.serviceInterface.ConsultationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ConsultationServiceImp implements ConsultaionService {
+public class ConsultationServiceImp implements ConsultationService {
+
+    @Autowired
+    ConsultationRepository consultationRepository;
+
+    @Override
+    public Consultation saveConsultation(Consultation consultation) {
+        return consultationRepository.save(consultation);
+    }
+
+    @Override
+    public Consultation getConsultationById(Long conId) {
+        return consultationRepository.findById(conId).get();
+    }
+
+    @Override
+    public void deleteConsultation(Long conId) {
+        consultationRepository.deleteById(conId);
+    }
+
+    @Override
+    public List<Consultation> getAllConsultations() {
+        return consultationRepository.findAll();
+    }
 }
